884fa8c6a552132a9270681ee65e0efae0f05145
hayleys9525 <hayleys.9525@gmail.com>
2023-03-06 17:38:02 +0900

    230306

---
+++ day06/src/inherit2/App3.java
@@ -0,0 +1,15 @@
+package inherit2;
+
+public class App3 {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		Employee e2 = new Manager();
+		Employee e3 = new Sales();
+
+		Object obj1 = new Employee();
+		Object obj2 = new Manager();
+		
+	}
+
+}
